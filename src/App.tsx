b50abd6b6aabefd1a7822c2b3a58da919e01194f
Jettsyy <teazyyy2512@gmail.com>
2025-02-21 08:50:34 +0700

    fix: delete router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { Route, Routes } from "react-router";
-import ListProduct from "./components/admin/ListProduct";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
+
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<ListProduct />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+    {/* Cấu hình router ở đây */}
     </>
   );
 }
